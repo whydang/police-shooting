59ff3182d5ce6344e1a0caecd953a80f78e3f976
Ying Dang <whydang@uw.edu>
2015-10-20 01:43:59 -0700

    omg it works!

--- js/map.js
+++ js/map.js
@@ -9,19 +9,18 @@ var drawMap = function() {
 	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
  
   	// Execute your function to get data
-  	getData();
+  	getData(map);
 }
 
 // Function for getting data
-var getData = function() {
-	console.log("hello");
+var getData = function(map) {
 	// Execute an AJAX request to get the data in data/response.js
 	$.ajax({
 	  url: 'data/response.json',
 	  data: 'hmm',
 	  success: function(data, jqXHR, textStatus) {
 	  	alert('AJAX call complete ' + data[0].County);
-	  		customBuild(data);
+	  		customBuild(map, data);
 	  },
 	  dataType: 'json'
 	});
@@ -31,30 +30,40 @@ var getData = function() {
 }
 
 // Loop through your data and add the appropriate layers and points
-var customBuild = function(data) {
+var customBuild = function(map, data) {
 	// Be sure to add each layer to the map
-	var Unkown = new L.LayerGroup([]);
-	var White = new L.LayerGroup([]);
-	var Black = new L.LayerGroup([]);
-	var Asian = new L.LayerGroup([]);
-	var Indian = new L.LayerGroup([]);
-	var Hawaiian = new L.LayerGroup([]);
+	var raceObj = {
+		"Unknown": new L.LayerGroup([]),
+		"White": new L.LayerGroup([]),
+		"Black or African American": new L.LayerGroup([]),
+		"Asian": new L.LayerGroup([]),
+		"American Indian or Alaska Native": new L.LayerGroup([]),
+		"Native Hawaiian or Other Pacific Islander": new L.LayerGroup([])
+	};
+
+	//raceObj["Unknown"].addTo(map);
 
 	for (var i = 0; i < data.length; i++) {
-		var raceValue = data[i].Race;
+		var raceValue = data[i].Race || "Unknown";
 		var longitude = data[i].lng;
 		var latitude = data[i].lat;
 
 		var circle = new L.circleMarker([latitude, longitude]).bindPopup(data[i].Summary);
-		//circle.setStyle({fillColor: #FF0000});
-
-		if (raceValue == null) {
-			circle.addTo(Unkown);
+		if (data[i]["Victim's Gender"] == 'Female') {
+			circle.setStyle({fillColor: '#FF0000'});
+		} else {
+			circle.setStyle({fillColor: '#001EFF'});
 		}
+
+		circle.addTo(raceObj[raceValue]);
+	}
+
+	for(var key in raceObj) {
+		raceObj[key].addTo(map);
 	}
 
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+	L.control.layers(null, raceObj).addTo(map);
 }
 
 
